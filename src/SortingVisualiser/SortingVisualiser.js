5a452188c666d03e0b0a9cf846e9f42258f3bae7
Eoin Pinaqui <45332521+eoinpinaqui@users.noreply.github.com>
2020-08-28 18:50:45 +0100

    Updated buttons

--- src/SortingVisualiser/SortingVisualiser.js
+++ src/SortingVisualiser/SortingVisualiser.js
@@ -120,7 +120,7 @@ export default class SortingVisualiser extends React.Component {
                         style={{height: `${value}px`}}/>
                 ))}
                 <div className="menu" style={{display: "block"}}>
-                    <button className="button arrayButton" onClick={() => this.resetArray()}>Generate New Array</button>
+                    <button className="button arrayButton" onClick={() => this.resetArray()}>New Array</button>
                     <button className="button sortButton" onClick={() => this.quickSort()}>Quick Sort</button>
                     <button className="button sortButton" onClick={() => this.mergeSort()}>Merge Sort</button>
                     <button className="button sortButton" onClick={() => this.timSort()}>Tim Sort</button>
